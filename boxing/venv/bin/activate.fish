74af43dcbf9878cbe8ebcd900d8febf32b23c832
briancapozza-bu <bfc0503@bu.edu>
2025-04-18 12:48:45 -0400

    added venv again

--- boxing/venv/bin/activate.fish
+++ boxing/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/briancapozza/CS411-travis-falk/boxing/venv
+set -gx VIRTUAL_ENV /home/briancapozza/CS411-HW3/boxing/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
